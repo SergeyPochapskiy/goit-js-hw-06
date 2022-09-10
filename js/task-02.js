bc77924e0d617554b9dc700aa91e7e89db8a4772
SergeyPochapskiy <104765308+SergeyPochapskiy@users.noreply.github.com>
2022-09-10 14:25:42 +0300

    Update task-02.js
    add-2 task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const IngredientsRef = document.querySelector('#ingredients')
+const liArray = []
+
+ingredients.forEach(ingredient => {
+	const item = document.createElement('li')
+	item.textContent = ingredient
+  item.className = 'item'
+	liArray.push(item)
+})
+
+
+IngredientsRef.append(...liArray)
