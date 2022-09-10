bf06cf09856b1907ad282cc13f477c47f8079077
SergeyPochapskiy <104765308+SergeyPochapskiy@users.noreply.github.com>
2022-09-10 14:26:55 +0300

    Update task-09.js
    add-9 task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+const changeBtnRef = document.querySelector('button.change-color')
+const bodyRef = document.querySelector('body')
+const colorValueRef = document.querySelector('span.color')
+
+changeBtnRef.addEventListener('click', event => {
+  let color = getRandomHexColor();
+  bodyRef.style.backgroundColor = color;
+  colorValueRef.textContent = color;
+})
